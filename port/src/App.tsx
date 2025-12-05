f6413ed94b651af9f2897a07f0b30c3a09e2107c
yadav412 <yadavsingh3012@gmail.com>
2025-12-04 16:02:50 -0800

    deployment fix

--- port/src/App.tsx
+++ port/src/App.tsx
@@ -19,7 +19,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<ProjectPage />} />
-        <Route path="" element={} />
       </Routes>
     </BrowserRouter>
   );
